b49c4ba39779786151ceebde28fcc1708911ea95
leonardobussi <leonardobezerrabussy@gmail.com>
2021-05-10 18:29:22 -0300

    add cors

--- config/initialize.js
+++ config/initialize.js
@@ -4,10 +4,12 @@ const consortium = require('../src/router/consortium');
 const investment = require('../src/router/investments');
 const users = require('../src/router/users');
 const test = require('../src/router/test');
+const cors = require('cors')
 const bodyParser = require('body-parser');
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
+app.use(cors())
 
 app.use('/consortium', consortium);
 app.use('/investment', investment);
