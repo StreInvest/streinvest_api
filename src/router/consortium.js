b3c8b42b5e59471bac76ae1c11ef287f3b8a0da1
leonardobussi <leonardobezerrabussy@gmail.com>
2021-06-14 22:42:08 -0300

    rota para o usuario especifico

--- src/router/consortium.js
+++ src/router/consortium.js
@@ -6,6 +6,7 @@ const router = express.Router();
 
 
 router.post('/:token', Product.postConsortium)
+router.get('specific/:token/:id', Product.getConsortiumEspecifico)
 router.get('/:token', Product.getConsortium)
 router.put('/:token/:id', Product.putConsortium)
 router.delete('/:token/:id', Product.deleteConsorcio)
